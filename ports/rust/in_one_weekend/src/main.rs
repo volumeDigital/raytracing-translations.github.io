009bee50494d26eddf65102d7aca70bd0a9ac2df
Tim Cussins <volumedigital@eml.cc>
2023-10-26 16:41:15 +1300

    Listing 8.

--- ports/rust/in_one_weekend/src/main.rs
+++ ports/rust/in_one_weekend/src/main.rs
@@ -1,10 +1,40 @@
 use in_one_weekend::color::*;
+use in_one_weekend::ray::*;
+use in_one_weekend::vec3::*;
+
+fn ray_color(_r: &Ray) -> Color {
+    Color::new(0.0, 0.0, 0.0)
+}
 
 fn main() {
     // Image
 
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
+
+    // Camera
+
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -13,12 +43,12 @@ fn main() {
     for j in 0..image_height {
         eprintln!("\rScanlines remaining: {}", image_height - j);
         for i in 0..image_width {
-            let pixel_color = Color::new(
-                i as f64 / ((image_width - 1) as f64),
-                j as f64 / ((image_height - 1) as f64),
-                0.0
-            );
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
 
+            let pixel_color = ray_color(&r);
             write_color(pixel_color);
         }
     }
